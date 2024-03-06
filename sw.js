04720938678edc4c02ae8ce98d82a0920493a475
Kissa Leon Denis <61694317+Happy-Lama@users.noreply.github.com>
2024-03-06 12:41:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-// const websocket = new WebSocket( 'ws://localhost:8000/ws/notifications/')
+const websocket = new WebSocket( 'wss://fyp-server-django.onrender.com/ws/notifications/')
 
-// websocket.addEventListener('open', (event) => {
-//     console.log("Websocket connection open")
-// })
-
-// websocket.addEventListener('close', () => {
-//     console.log("Websocket connection closed")
-// })
-
-
-// websocket.addEventListener('message', (event) => {
-//     console.log(event)
-    
-//     // const notificationData = event.data
-
-//     // const options = {
-//     //     body: notificationData,
-//     // }
-//     // console.log(self.registration)
-//     self.registration.showNotification("Notification", {
-//         body: "Hello",
-//     })
-// })
-
-self.registration.showNotification("Notification", {
-    body: "Hello",
+websocket.addEventListener('open', (event) => {
+    console.log("Websocket connection open")
 })
+
+websocket.addEventListener('close', () => {
+    console.log("Websocket connection closed")
+})
+
+
+websocket.addEventListener('message', (event) => {
+    console.log(event)
+    
+    // const notificationData = event.data
+
+    // const options = {
+    //     body: notificationData,
+    // }
+    // console.log(self.registration)
+    self.registration.showNotification("Notification", {
+        body: "Hello",
+    })
+})
+
+// self.registration.showNotification("Notification", {
+//     body: "Hello",
+// })
