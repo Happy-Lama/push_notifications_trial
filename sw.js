0ef0dd6f23f6ee4d24f57c67309dafc6d06451f5
Kissa Leon Denis <61694317+Happy-Lama@users.noreply.github.com>
2024-03-06 12:11:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,28 @@
-const websocket = new WebSocket( 'ws://localhost:8000/ws/notifications/')
+// const websocket = new WebSocket( 'ws://localhost:8000/ws/notifications/')
 
-websocket.addEventListener('open', (event) => {
-    console.log("Websocket connection open")
-})
+// websocket.addEventListener('open', (event) => {
+//     console.log("Websocket connection open")
+// })
 
-websocket.addEventListener('close', () => {
-    console.log("Websocket connection closed")
-})
+// websocket.addEventListener('close', () => {
+//     console.log("Websocket connection closed")
+// })
 
 
-websocket.addEventListener('message', (event) => {
-    console.log(event)
+// websocket.addEventListener('message', (event) => {
+//     console.log(event)
     
-    // const notificationData = event.data
+//     // const notificationData = event.data
 
-    // const options = {
-    //     body: notificationData,
-    // }
-    // console.log(self.registration)
-    self.registration.showNotification("Notification", {
-        body: "Hello",
-    })
+//     // const options = {
+//     //     body: notificationData,
+//     // }
+//     // console.log(self.registration)
+//     self.registration.showNotification("Notification", {
+//         body: "Hello",
+//     })
+// })
+
+self.registration.showNotification("Notification", {
+    body: "Hello",
 })
